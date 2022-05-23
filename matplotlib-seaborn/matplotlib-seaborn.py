82854f3eb5c33795b94fda2196bbea6f78ce913f
TiagoFeu <tiago.hartur@gmail.com>
2022-05-23 16:54:30 -0300

    2 plot

--- matplotlib-seaborn/matplotlib-seaborn.py
+++ matplotlib-seaborn/matplotlib-seaborn.py
@@ -5,13 +5,18 @@ import seaborn as sns
 
 covid_panel = pd.read_csv("../data/microdados.csv", encoding="ISO-8859-1", on_bad_lines="skip", sep=";", parse_dates=["DataDiagnostico","DataObito"])
 
-
 # Evolution of cases
 confirmed = covid_panel[covid_panel['Classificacao'] == 'Confirmados']
 confirmed_by_day = confirmed.groupby(['DataDiagnostico']).size()
 confirmed_sum = confirmed_by_day.cumsum()
 
-plot = sns.lineplot(x=confirmed_sum.index, y=confirmed_sum)
+# Evolution of deaths
+dead = covid_panel[covid_panel['Evolucao'] == 'Óbito pelo COVID-19']
+dead_by_day = dead.groupby(['DataObito']).size()
+dead_sum = dead_by_day.cumsum()
+
+plot = sns.lineplot(x=confirmed_sum.index, y=confirmed_sum, color='red')
+plot = sns.lineplot(x=dead_sum.index, y=dead_sum, color='blue')
 fig = plot.get_figure()
 fig.savefig("../results/confirmed_sum.png")
 
