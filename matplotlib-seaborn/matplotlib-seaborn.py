23650dc3917381cc37a0e92e3e2fb6e1e38dab0c
TiagoFeu <tiago.hartur@gmail.com>
2022-05-23 15:10:09 -0300

    starting

--- matplotlib-seaborn/matplotlib-seaborn.py
+++ matplotlib-seaborn/matplotlib-seaborn.py
@@ -1,1 +1,5 @@
-import matplotlib
+import pandas as pd
+import datetime as date
+
+covid_panel = pd.read_csv("../data/microdados.csv", encoding="ISO-8859-1", on_bad_lines="skip", sep=";", parse_dates=["DataDiagnostico","DataObito"])
+
